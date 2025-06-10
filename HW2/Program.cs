54254460f45521a9ddc1fff0c60ccf28c72edb02
Ilyaivashkov <ilyaivashkov.code@gmail.com>
2025-06-10 13:42:41 +0300

    Fixed Lab N2

---
+++ HW2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MusicApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var playlist = new Playlist("My Favorite Songs", 5);
+            playlist.AddSong("Imagine");
+            playlist.AddSong("Bohemian Rhapsody");
+            playlist.AddSong("Stairway to Heaven");
+
+            Console.WriteLine(playlist.GetPlaylistInfo());
+
+            playlist.RemoveSong("Imagine");
+
+            Console.WriteLine(playlist.GetPlaylistInfo());
+        }
+    }
+}
